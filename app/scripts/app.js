c06f8fdc0663e4053f90de946c1741a734ea642d
Arthur Bouie <arthurbouie@gmail.com>
2014-06-03 22:18:41 -0400

    NG route figured out and created element directive

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,16 +1,25 @@
 'use strict';
 
-angular
-  .module('abouieApp', [
-    'ngRoute'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+  angular.module('abouieApp', ['ngRoute'])
+    .config(function ($routeProvider) {
+      $routeProvider
+        .when('/', {
+          templateUrl: 'views/main.html',
+          controller: 'MainCtrl'
+        })
+        .when('/about', {
+          templateUrl: 'views/about.html',
+          // controller('MainCtrl'
+        })
+        .otherwise({
+          redirectTo: '/'
+        });
+    })
+    .directive('mainLinks', function(){
+      // Runs during compile
+      return {
+        restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
+        templateUrl: 'views/main-links.html',
+      };
+    })
+    ;
